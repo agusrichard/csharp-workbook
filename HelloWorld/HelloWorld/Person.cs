e6b564c7a1d171e292a1b42c870ad623829c8de9
Agus Richard Lubis <agus.richard21@gmail.com>
2022-03-14 15:04:32 +0700

    Complete learning C Sharp Fundemantal

---
+++ HelloWorld/HelloWorld/Person.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Person
+    {
+        private string name;
+        private int age;
+        private static int numberOfPersons;
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+
+            Person.numberOfPersons++;
+        }
+
+        public Person()
+        {
+            this.name = "";
+            this.age = 0;
+            Person.numberOfPersons++;
+        }
+
+        public override string ToString()
+        {
+            return $"My name is {this.name} and I am {this.age} years old!";
+
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+
+            set { this.name = value; }
+        }
+
+        public int Age
+        {
+            get { return this.age; }
+            set { this.age = value; }
+        }
+
+        public static int NumberOfPersons
+        {
+            get { return Person.numberOfPersons; }
+        }
+    }
+}
--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -10,8 +10,14 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
+            Person person1 = new Person("Sherlock Holmes", 25);
+            Console.WriteLine(person1.ToString());
+            Person person2 = new Person("John Watson", 21);
+            Console.WriteLine(person2.ToString());
+            Person person3 = new Person("Irene Adler", 20);
+            Console.WriteLine(person3.ToString());
+            Console.WriteLine(Person.NumberOfPersons);
+            Console.WriteLine(Util.Add(10, 11));
         }
     }
 }
